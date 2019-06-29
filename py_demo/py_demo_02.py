301013e3343cf9357003646e6b6b8a1ca13981be
ling0258 <1253517211@qq.com>
2019-06-29 12:23:28 +0800

    第三次作业

---
+++ py_demo/py_demo_02.py
@@ -0,0 +1,4 @@
+#水仙花数：水仙花数（Narcissistic number）也被称为超完全数字不变数（pluperfect digital invariant, PPDI）、自恋数、自幂数、阿姆斯壮数或阿姆斯特朗数（Armstrong number），
+# 水仙花数是指一个 n 位数（n≥3 ），它的每个位上的数字的 n 次幂之和等于它本身（例如：1^3 + 5^3+ 3^3 = 153）。
+
+
